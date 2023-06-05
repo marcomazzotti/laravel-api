1ae28462f81b8b9b21da1e91bfa4bb2cc2bf7d25
marcomazzotti <marcoalbertomazzotti@gmail.com>
2023-06-05 16:28:08 +0200

    Add: function index in ProjectController

---
+++ resources/views/admin/projects/index.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.admin')
+
+@section('content')
+    <h1>Tutti i progetti</h1>
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">ID</th>
+                <th scope="col">Titolo</th>
+                <th scope="col">Slug</th>
+                <th scope="col">Azioni</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($projects as $project)
+                <tr>
+                    <th scope="row">{{ $project->id }}</th>
+                    <td>{{ $project->title }}</td>
+                    <td>{{ $project->slug }}</td>
+                    <td></td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+@endsection
--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -54,18 +54,14 @@
                 <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-dark navbar-dark sidebar collapse">
                     <div class="position-sticky pt-3">
                         <ul class="nav flex-column">
-                            {{-- <li class="nav-item">
+                            <li class="nav-item">
                                 <a class="nav-link text-white {{ Route::currentRouteName() == 'admin.dashboard' ? 'bg-secondary' : '' }}"
-                                    href="{{ route('admin.dashboard') }}">
-                                    <i class="fa-solid fa-tachometer-alt fa-lg fa-fw"></i> Dashboard
-                                </a>
+                                    href="{{ route('admin.dashboard') }}">Dashboard</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link text-white {{ Route::currentRouteName() == 'admin.posts.index' ? 'bg-secondary' : '' }}"
-                                    href="{{ route('admin.posts.index') }}">
-                                    <i class="fa-regular fa-folder-open"></i> Post
-                                </a>
-                            </li> --}}
+                                <a class="nav-link text-white {{ Route::currentRouteName() == 'admin.projects.index' ? 'bg-secondary' : '' }}"
+                                    href="{{ route('admin.projects.index') }}">Progetti</a>
+                            </li>
                         </ul>
 
 
