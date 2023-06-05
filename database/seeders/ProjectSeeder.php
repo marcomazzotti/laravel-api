1daacf233ae7107a2013d19b155e1885f485f551
marcomazzotti <marcoalbertomazzotti@gmail.com>
2023-06-05 16:17:45 +0200

    Add: migration + seeder + ProjectController

---
+++ database/seeders/ProjectSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Project;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Str;
+use Faker\Generator as Faker;
+use Illuminate\Database\Seeder;
+
+class ProjectSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for ($i = 0; $i < 20; $i++) {
+            $project = new Project();
+            $project->title = $faker->sentence(3);
+            $project->slug = Str::slug($project->title, '-');
+            $project->content = $faker->text();
+            $project->save();
+        }
+    }
+}
