3c19e41619fbec7c569b4b098d5adeb952273f30
marcomazzotti <marcoalbertomazzotti@gmail.com>
2023-06-07 16:46:01 +0200

    Add: link between tables

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -10,4 +10,9 @@ class Project extends Model
     use HasFactory;
 
     protected $fillable = ["title", "content", "slug"];
+
+    public function type()
+    {
+        return $this->belongsTo(Type::class);
+    }
 }
