203184d8e08b03c843ad67612b680d53ca6ee659
marcomazzotti <marcoalbertomazzotti@gmail.com>
2023-06-07 16:53:37 +0200

    Add: type to show and index in views

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -1,6 +1,11 @@
 @extends('layouts.admin')
 @section('content')
     <h1>{{ $project->title }}</h1>
+    @if ($project->category)
+        <p>Categoria: {{ $project->type->name }}</p>
+    @else
+        <p>Nessun tipo</p>
+    @endif
     <p>{{ $project->slug }}</p>
     <p>{{ $project->content }}</p>
     <a class="btn btn-primary" href="{{ route('admin.projects.index') }}">Torna alla home</a>
